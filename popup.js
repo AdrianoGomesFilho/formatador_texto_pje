9a0f1b7706327b5f8e243798c6962ef247d6dca8
Adriano Gomes <adrianogomesfilho98@gmail.com>
2024-11-11 10:15:04 -0300

    mantendo projeto

--- popup.js
+++ popup.js
@@ -7,6 +7,8 @@ document.getElementById('formatButton').addEventListener('click', function() {
     .replace(/Assinado eletronicamente por:.*? - \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/g, '') // Remove entire sentence
     .replace(/QR Code para validação de documento.*? - \w{7}/g, '') // Remove QR Code validation string
     .replace(/Símbolo PJe QR Code para validação de documento/g, '') // Remove additional string
+    .replace(/^\s*[\r\n]+/gm, '') // Remove blank lines
+    .replace(/^\s*$/gm, '') // Remove lines that are only whitespace
     .trim();
   document.getElementById('output').value = formattedText;
 });
