7e4b34f799350a960c27246eedd2955820f917e6
Adriano Gomes <adrianogomesfilho98@gmail.com>
2024-11-14 14:37:57 -0300

    arrumando, é melhor remover double spaces, break lines primeiro, a logica só detecta com espaço unico

--- popup.js
+++ popup.js
@@ -13,7 +13,11 @@ document.getElementById('copyButton').addEventListener('click', function() {
 function formatText() {
   let inputText = document.getElementById('inputText').value;
   let formattedText = inputText
-  .replace(/Documento assinado eletronicamente por.*? - \w{7}/g, '')
+    .replace(/\s+/g, ' ') // Remove extra spaces
+    .replace(/\n/g, ' ') // Remove breaklines
+    .replace(/^\s*[\r\n]+/gm, '') // Remove blank lines
+    .replace(/^\s*$/gm, '') // Remove lines that are only whitespace
+    .replace(/Documento assinado eletronicamente por.*? - \w{7}/g, '')
     .replace(/Assinado eletronicamente por:.*? - \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/g, '')
     .replace(/Assinado eletronicamente por:.*? - Juntado em: \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/g, '')
     .replace(/QR Code para validação de documento.*? - \w{7}/g, '')
@@ -26,10 +30,6 @@ function formatText() {
     .replace(/https:\/\/pje\.trt\d{2}\.jus\.br\/(segundograu|primeirograu)\/Processo\/ConsultaDocumento\/listView\.seam\?nd=\d{29}/g, '') // Remove URL
     .replace(/ID\. \w{7} - Pág\. \d+/g, '') // Remove "ID. <id> - Pág. <number>"
     .replace(/\d{29}/g, '') // Remove 29-digit numbers  
-    .replace(/\s+/g, ' ') // Remove extra spaces
-    .replace(/\n/g, ' ') // Remove breaklines
-    .replace(/^\s*[\r\n]+/gm, '') // Remove blank lines
-    .replace(/^\s*$/gm, '') // Remove lines that are only whitespace
     .replace(/Fls\.\:\s*\d+/g, '') // Remove "Fls.: <number>"
     
     .trim();
