17678003c1b2adb560e57ed9a7610ff71e099e43
Adriano Gomes <adrianogomesfilho98@gmail.com>
2024-11-14 14:22:51 -0300

    melhorando a remoção do qr code do segundo grau que sai com mais informações

--- popup.js
+++ popup.js
@@ -23,6 +23,11 @@ function formatText() {
     .replace(/Assinado eletronicamente por:.*? - Juntado em: \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/g, '')
     .replace(/QR Code para validação de documento.*? - \w{7}/g, '')
     .replace(/Símbolo PJe QR Code para validação de documento/g, '')
+    .replace(/Número do processo: \d{7}-\d{2}\.\d{4}\.\d{1,2}\.\d{2}\.\d{4}/g, '') // Remove "Número do processo: <number>"
+    .replace(/Número do documento: \d{29}/g, '') // Remove "Número do documento: <number>"
+    .replace(/https:\/\/pje\.trt\d{2}\.jus\.br\/(segundograu|primeirograu)\/Processo\/ConsultaDocumento\/listView\.seam\?nd=\d{29}/g, '') // Remove URL
+    .replace(/ID\. \w{7} - Pág\. \d+/g, '') // Remove "ID. <id> - Pág. <number>"
+    .replace(/\d{26}/g, '') // Remove 26-digit numbers
     .trim();
   document.getElementById('outputText').value = formattedText;
 }
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Formatador de Texto PJE
+
+## Descrição
+O Formatador de Texto PJE é uma ferramenta desenvolvida para facilitar a formatação de textos no sistema PJE (Processo Judicial Eletrônico). Ele permite que os usuários formatem documentos de maneira rápida e eficiente.
+
+## Funcionalidades
+- Remoção de quebras de linha, ocupando menos espaço na citação
+- Remoção de linhas em branco
+- Remoção de espaços dobrados/triplicados
+- Removação de trechos com assinatura digital/QR CODE
+
+## Como Usar
+1. Baixar extensão via loja de aplicativos Google Chrome
+
+## Licença
+Este projeto está licenciado sob a Licença MIT. Veja o arquivo LICENSE para mais detalhes.
