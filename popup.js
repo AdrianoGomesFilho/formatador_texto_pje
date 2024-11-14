3f71566655ae5ba25d8bdda1b2c92e5039e7bbf7
Adriano Gomes <adrianogomesfilho98@gmail.com>
2024-11-14 14:33:17 -0300

    resolvendo replace, ordenando

--- popup.js
+++ popup.js
@@ -13,12 +13,7 @@ document.getElementById('copyButton').addEventListener('click', function() {
 function formatText() {
   let inputText = document.getElementById('inputText').value;
   let formattedText = inputText
-    .replace(/\s+/g, ' ') // Remove extra spaces
-    .replace(/\n/g, ' ') // Remove breaklines
-    .replace(/^\s*[\r\n]+/gm, '') // Remove blank lines
-    .replace(/^\s*$/gm, '') // Remove lines that are only whitespace
-    .replace(/Fls\.\:\s*\d+/g, '') // Remove "Fls.: <number>"
-    .replace(/Documento assinado eletronicamente por.*? - \w{7}/g, '')
+  .replace(/Documento assinado eletronicamente por.*? - \w{7}/g, '')
     .replace(/Assinado eletronicamente por:.*? - \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/g, '')
     .replace(/Assinado eletronicamente por:.*? - Juntado em: \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/g, '')
     .replace(/QR Code para validação de documento.*? - \w{7}/g, '')
@@ -30,7 +25,13 @@ function formatText() {
     .replace(/\d{29}/g, '') // Remove 29-digit numbers
     .replace(/https:\/\/pje\.trt\d{2}\.jus\.br\/(segundograu|primeirograu)\/Processo\/ConsultaDocumento\/listView\.seam\?nd=\d{29}/g, '') // Remove URL
     .replace(/ID\. \w{7} - Pág\. \d+/g, '') // Remove "ID. <id> - Pág. <number>"
-    .replace(/\d{29}/g, '') // Remove 29-digit numbers
+    .replace(/\d{29}/g, '') // Remove 29-digit numbers  
+    .replace(/\s+/g, ' ') // Remove extra spaces
+    .replace(/\n/g, ' ') // Remove breaklines
+    .replace(/^\s*[\r\n]+/gm, '') // Remove blank lines
+    .replace(/^\s*$/gm, '') // Remove lines that are only whitespace
+    .replace(/Fls\.\:\s*\d+/g, '') // Remove "Fls.: <number>"
+    
     .trim();
   document.getElementById('outputText').value = formattedText;
 }
