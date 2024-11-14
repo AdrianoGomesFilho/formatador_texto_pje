d6c4068060cd20d087ce1c3145cb0669af5cc8b0
Adriano Gomes <adrianogomesfilho98@gmail.com>
2024-11-14 14:31:09 -0300

    consertando o replace

--- popup.js
+++ popup.js
@@ -13,6 +13,11 @@ document.getElementById('copyButton').addEventListener('click', function() {
 function formatText() {
   let inputText = document.getElementById('inputText').value;
   let formattedText = inputText
+    .replace(/\s+/g, ' ') // Remove extra spaces
+    .replace(/\n/g, ' ') // Remove breaklines
+    .replace(/^\s*[\r\n]+/gm, '') // Remove blank lines
+    .replace(/^\s*$/gm, '') // Remove lines that are only whitespace
+    .replace(/Fls\.\:\s*\d+/g, '') // Remove "Fls.: <number>"
     .replace(/Documento assinado eletronicamente por.*? - \w{7}/g, '')
     .replace(/Assinado eletronicamente por:.*? - \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/g, '')
     .replace(/Assinado eletronicamente por:.*? - Juntado em: \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/g, '')
@@ -21,13 +26,11 @@ function formatText() {
     .replace(/Número do processo: \d{7}-\d{2}\.\d{4}\.\d{1,2}\.\d{2}\.\d{4}/g, '') // Remove "Número do processo: <number>"
     .replace(/Número do documento: \d{29}/g, '') // Remove "Número do documento: <number>"
     .replace(/https:\/\/pje\.trt\d{2}\.jus\.br\/(segundograu|primeirograu)\/Processo\/ConsultaDocumento\/listView\.seam\?nd=\d{29}/g, '') // Remove URL
+    .replace(/ID\. \w{8} - Pág\. \d+/g, '') // Remove "ID. <id> - Pág. <number>"
+    .replace(/\d{29}/g, '') // Remove 29-digit numbers
+    .replace(/https:\/\/pje\.trt\d{2}\.jus\.br\/(segundograu|primeirograu)\/Processo\/ConsultaDocumento\/listView\.seam\?nd=\d{29}/g, '') // Remove URL
     .replace(/ID\. \w{7} - Pág\. \d+/g, '') // Remove "ID. <id> - Pág. <number>"
-    .replace(/\d{26}/g, '') // Remove 26-digit numbers  
-    .replace(/\s+/g, ' ') // Remove extra spaces
-    .replace(/\n/g, ' ') // Remove breaklines
-    .replace(/^\s*[\r\n]+/gm, '') // Remove blank lines
-    .replace(/^\s*$/gm, '') // Remove lines that are only whitespace
-    .replace(/Fls\.\:\s*\d+/g, '') // Remove "Fls.: <number>"
+    .replace(/\d{29}/g, '') // Remove 29-digit numbers
     .trim();
   document.getElementById('outputText').value = formattedText;
 }
