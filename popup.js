af04543b49a7ce65af6851c2f1cb87d8f78603d2
Adriano Gomes <69825351+AdrianoGomesFilho@users.noreply.github.com>
2024-11-24 15:49:36 -0300

    finalizando estudos e melhoramentos

--- popup.js
+++ popup.js
@@ -11,57 +11,47 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function formatText() {
   let inputText = document.getElementById('inputText').value;
+  let formattedText = inputText;
 
-  // Remove espaços extras
-  let formattedText = inputText.replace(/\s+/g, ' ');
+  const patternsToRemove = [
+    // Remove extra spaces
+    { pattern: /\s+/g, replacement: ' ' },
+    // Remove "Número do processo: <número>"
+    { pattern: /Número do processo:\s*\d{7}-\d{2}\.\d{4}\.\d{1,2}\.\d{2}\.\d{4}/gi, replacement: '' },
+    // Remove "Número do documento: <número>"
+    { pattern: /Número do documento:\s*\d{29}/gi, replacement: '' },
+    // Remove URLs
+    { pattern: /https:\/\/pje\.trt\d+\.jus\.br\/\w+\/Processo\/ConsultaDocumento\/listView\.seam\?nd=\d+/gi, replacement: '' },
+    // Remove "Assinado eletronicamente por: <nome> - <data> - <id>"
+    { pattern: /Assinado eletronicamente por:.*? - \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/gi, replacement: '' },
+    // Remove "Documento assinado eletronicamente por <nome>, em <data>, às <hora> - <id>"
+    { pattern: /Documento assinado eletronicamente por .*?, em \d{2}\/\d{2}\/\d{4}, às \d{2}:\d{2}:\d{2} - \w{7}/gi, replacement: '' },
+    // Remove "Documento assinado eletronicamente por <nome>, em <data>, às <hora> - <id> Fls.: <número>"
+    { pattern: /Documento assinado eletronicamente por .*?, em \d{2}\/\d{2}\/\d{4}, às \d{2}:\d{2}:\d{2} - \w{7} Fls\.\: \d+/gi, replacement: '' },
+    // Remove "ID. <id> - Pág. <número>"
+    { pattern: /ID\. \w{7,8} - Pág\. \d+/gi, replacement: '' },
+    // Remove "Fls.: <número>"
+    { pattern: /Fls\.\:\s*\d+/gi, replacement: '' },
+    // Remove line breaks
+    { pattern: /\n/g, replacement: ' ' },
+    // Remove blank lines
+    { pattern: /^\s*[\r\n]+/gm, replacement: '' },
+    // Remove lines that are only whitespace
+    { pattern: /^\s*$/gm, replacement: '' },
+    // Remove any remaining extra spaces
+    { pattern: /\s+/g, replacement: ' ' }
+  ];
 
-  // Remove "Número do processo: <número>"
-  formattedText = formattedText.replace(/Número do processo:\s*\d{7}-\d{2}\.\d{4}\.\d{1,2}\.\d{2}\.\d{4}/gi, '');
- 
+  patternsToRemove.forEach(({ pattern, replacement }) => {
+    formattedText = formattedText.replace(pattern, replacement);
+  });
 
-  // Remove "Número do documento: <número>"
-  formattedText = formattedText.replace(/Número do documento:\s*\d{29}/gi, '');
-
-
-  // Remove URLs mesmo que façam parte de uma linha mais longa
-  formattedText = formattedText.replace(/https:\/\/pje\.trt\d+\.jus\.br\/\w+\/Processo\/ConsultaDocumento\/listView\.seam\?nd=\d+/gi, '');
-
-  // Remove "Assinado eletronicamente por: <nome> - <data> - <id>"
-  formattedText = formattedText.replace(/Assinado eletronicamente por:.*? - \d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} - \w{7}/gi, '');
-
-  // Remove "Documento assinado eletronicamente por <nome>, em <data>, às <hora> - <id>", o quantificador non greedy (com interrogação) .*? garante que o restante da expressão seja detectado ao pé da letra'
-  formattedText = formattedText.replace(/Documento assinado eletronicamente por .*?, em \d{2}\/\d{2}\/\d{4}, às \d{2}:\d{2}:\d{2} - \w{7}/gi, '');
-
-  // Remove "Documento assinado eletronicamente por <nome>, em <data>, às <hora> - <id> Fls.: <número>"
-  formattedText = formattedText.replace(/Documento assinado eletronicamente por .*?, em \d{2}\/\d{2}\/\d{4}, às \d{2}:\d{2}:\d{2} - \w{7} Fls\.\: \d+/gi, '');
-
-  // Remove "ID. <id> - Pág. <número>"
-  formattedText = formattedText.replace(/ID\. \w{7,8} - Pág\. \d+/gi, '');
-
-  // Remove "Fls.: <número>"
-  formattedText = formattedText.replace(/Fls\.\:\s*\d+/gi, '');
-
-  // Remove quebras de linha
-  formattedText = formattedText.replace(/\n/g, ' ');
-
-  // Remove linhas em branco
-  formattedText = formattedText.replace(/^\s*[\r\n]+/gm, '');
-
-  // Remove linhas que são apenas espaços em branco
-  formattedText = formattedText.replace(/^\s*$/gm, '');
-
-  // Remove quaisquer espaços extras restantes após as substituições
-  formattedText = formattedText.replace(/\s+/g, ' ').trim();
-
-
-  // Define o texto formatado no elemento de saída
+  formattedText = formattedText.trim();
   document.getElementById('outputText').value = formattedText;
 }
 
 // Função para copiar o texto formatado para a área de transferência
 function copyText() {
   let outputText = document.getElementById('outputText');
-  outputText.select();
-  outputText.setSelectionRange(0, 99999); // Para dispositivos móveis
-  document.execCommand('copy');
+  navigator.clipboard.writeText(outputText.value);
 }
